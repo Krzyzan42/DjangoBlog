55efa20bb90f93af247edcd20ddc18c6bdbd9e5a
Jakub Krzyzanowski <jakubkrzyzanowski42@gmail.com>
2023-11-15 11:31:45 +0100

    Changed settings for deployment

--- studybud/settings.py
+++ studybud/settings.py
@@ -21,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7t8i%=_#@!l)g5jkd3k=ds5xo)hnr0%8=#=jzug16m+d!g1ig8'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
